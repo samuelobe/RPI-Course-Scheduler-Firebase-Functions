72714684b091dfe2e6383f93dcbfa3de4c460d0a
Samuel Obe <obes@rpi.edu>
2020-05-28 17:21:00 -0400

    Retrieved text values for every offered class

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,17 @@
-import * as functions from 'firebase-functions';
-import * as rp from 'request-promise';
+import * as functions from "firebase-functions";
+import * as axios from "axios";
+import * as cheerio from "cheerio";
 
-const url = 'https://sis.rpi.edu/reg/zs20200501.htm';
+const url = "https://sis.rpi.edu/reg/zs20200501.htm";
 
 export const getHTML = functions.https.onRequest((request, response) => {
-    rp(url)
-        .then(function (html) {
-            //success!
-            response.send(html);
-            console.log(html);
-        })
-        .catch(function (err) {
-            //handle error
-        });
-
+  axios.default
+    .get(url)
+    .then((res) => {
+      const $ = cheerio.load(res.data);
+      
+      response.send($("tr").text());
+      //console.log(res.data);
+    })
+    .catch(() => console.log("poop"));
 });
