50ed6abf1b6d34c36983d601ed15cd1446f69b0b
Samuel Obe <obes@rpi.edu>
2020-06-02 14:37:33 -0400

    Initial fix-dev commit

--- functions/main.py
+++ functions/main.py
@@ -3,15 +3,30 @@ import time
 import csv
 import json
 import sys
-from flask import jsonify
-from collections import OrderedDict
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 
 def test(request):
     start_time = time.time()
 
-    open('/tmp/data.csv', 'w')
-    csvfile = open('/tmp/data.csv', 'r')
+    project_id = 'course-scheduler-e52d1'
+    # Use the application default credentials
+    #cred = credentials.ApplicationDefault()
+    cred = credentials.Certificate(
+        "api_key.json")
+
+    firebase_admin.initialize_app(cred, {
+        'projectId': project_id,
+    })
+
+    db = firestore.client()
+
+    # Create temp file for GCP
+
+    csvfile = open('/tmp/data.csv', 'w')
 
     # The webpage URL whose table we want to extract
     url = 'https://sis.rpi.edu/reg/zs20200501.htm'
@@ -33,26 +48,62 @@ def test(request):
 
     time.sleep(1)
 
-    fieldnames = ("Course-Sec CRN", "Course Title", "Class Type", "Credit Hours", "Grade Type", "Class Days",
-                  "Start Time", "End Time", "Instructor", "Max Enrollment", "Seats Taken", "Seats Remaining")
+    csvfile = open('/tmp/data.csv', 'r')
 
-    reader = csv.DictReader(csvfile, fieldnames)
+    r = csv.reader(csvfile)
+    csv_lines = list(r)
+    length = len(csv_lines)
 
-    courses = {"courses": []}
-
-    i = 0
-    for row in reader:
+    for i in range(length):
         if i >= 2:
-            sorted_row = OrderedDict(sorted(row.items(),
-                                            key=lambda item: reader.fieldnames.index(item[0])))
-            courses['courses'].append(sorted_row)
-        i += 1
+            if csv_lines[i][0] == '' and csv_lines[i][1] == '':
+                csv_lines[i][0] = csv_lines[i-1][0]
+                csv_lines[i][1] = csv_lines[i-1][1]
 
+    csv_lines.remove(csv_lines[0])
+    csv_lines.remove(csv_lines[0])
+
+    dict_tuple = ('CRN Course-Sec', 'Course Title', 'Class Type', 'Credit Hrs', 'Gr Tp',
+                  'Class Days', 'Start Time', 'End Time', 'Instructor', 'Max Enrl', 'Enrl', 'Sts Rmng')
+
+    for line in csv_lines:
+        if 'NOTE:' in line[1]:
+            csv_lines.remove(line)
+
+    courses = {"courses": {}}
+
+    for line in csv_lines:
+        courses['courses'][line[0].split()[1].decode('utf8')] = {
+            dict_tuple[1].decode('utf8'): line[1].decode('utf8'),
+            dict_tuple[2].decode('utf8'): line[2].decode('utf8'),
+            dict_tuple[3].decode('utf8'): line[3].decode('utf8'),
+            dict_tuple[4].decode('utf8'): line[4].decode('utf8'),
+            dict_tuple[5].decode('utf8'): line[5].decode('utf8'),
+            dict_tuple[6].decode('utf8'): line[6].decode('utf8'),
+            dict_tuple[7].decode('utf8'): line[7].decode('utf8'),
+            dict_tuple[8].decode('utf8'): line[8].decode('utf8'),
+            dict_tuple[9].decode('utf8'): line[9].decode('utf8'),
+            dict_tuple[10].decode('utf8'): line[10].decode('utf8'),
+            dict_tuple[11].decode('utf8'): line[11].decode('utf8'),
+        }
+
+    # Add courses to Firestore
+    doc_ref = db.collection('courses')
+
+    for course in courses['courses']:
+        doc_ref.document(course).set(courses['courses'][course])
+
+    """
     r = json.dumps(courses)
+    test = open('data.json', 'w')
+    test.write(r)
+    """
+
+    csvfile.close()
     end_time = time.time()
     return r, 200, {'Content-Type': 'application/json'}
 
-"""
+
 if __name__ == "__main__":
-    test()
-"""
+    test('skrt')
+    os.remove("/tmp/data.csv")
